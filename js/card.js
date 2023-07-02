16261b39a92a7ae96e4d328286589fd2239442f2
Kordasauter <98216308+Kordasauter@users.noreply.github.com>
2023-07-02 16:58:00 +0200

    Jitterbug card combo effect added

--- js/card.js
+++ js/card.js
@@ -694,7 +694,7 @@ function SetCard()
 ,[625,	 card_com_ACC_R,"Playing Pere",0,bon_DMG_ELE_NEUTRAL,20,0]
 ,[626,	 card_com_ACC_L,"Singing Pere",0,bon_DMG_ELE_NEUTRAL,20,0]
 ,[627,	 itm_type_SET,"Playing Pere + Singing Pere",0,bon_DMG_SKILL + skill_MIWA_METALLIC_SOUND,50,0]
-,[628,	 itm_type_SET,"Playing Pere + Singing Pere + Jitterbug","Has a chance to transform into [Jitterbug] for 6 sec. when attacks.",0]
+,[628,	 itm_type_SET,"Playing Pere + Singing Pere + Jitterbug","Has a chance to transform into [Jitterbug] for 6 sec. when attacks.(always active in the simulator)",bon_DMG_ELE_NEUTRAL,200,bon_MDMG_ELE_NEUTRAL,200,0]
 //Faceworm Nest
 ,[629,	 card_com_ACC,"Faceworm",0,bon_AGI,-1,bon_DEX,-1,bon_ASPD_MUL,3,0]
 ,[630,	 card_com_ACC,"Faceworm Dark",0,bon_VIT,-1,bon_INT,-1,bon_RED_CASTDELAY,3,0]
